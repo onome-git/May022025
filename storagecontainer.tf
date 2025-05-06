955b70bd4e43b547fb8aac4c551b53e82a737bd9
onome-git <reghaonome@gmail.com>
2025-05-06 13:06:24 -0400

    Update storagecontainer.tf

--- storagecontainer.tf
+++ storagecontainer.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "onomecontainer" {
   name                  = "content"
-  storage_account_id    = azurerm_storage_account.onomecontainer.id
+  storage_account_id    = azurerm_storage_account.mcitstorage.id
   container_access_type = "private"
 }
