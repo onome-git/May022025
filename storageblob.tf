745d76f513fb83e85e4efabed4522f4d4e92e106
onome-git <reghaonome@gmail.com>
2025-05-05 20:58:59 -0400

    Create storageblob.tf

---
+++ storageblob.tf
@@ -0,0 +1,7 @@
+resource "azurerm_storage_blob" "example" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.mcitstorage.name
+  storage_container_name = azurerm_storage_container.mcitstorage.name
+  type                   = "Block"
+  source                 = "some-local-file.zip"
+}
