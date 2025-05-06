fa9554d8168ef96578cc295cff1a9e484074cde7
onome-git <reghaonome@gmail.com>
2025-05-05 20:41:14 -0400

    Update storagecontainer.tf

--- storagecontainer.tf
+++ storagecontainer.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "example" {
   name                  = "content"
-  storage_account_id    = "azurerm_storage_account.example.id"
+  storage_account_id    = azurerm_storage_account.example.id
   container_access_type = "private"
 }
