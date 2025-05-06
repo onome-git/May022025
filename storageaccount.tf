bbcf0d3c9d4141c3b711b13cbb908c74c9f55388
onome-git <reghaonome@gmail.com>
2025-05-05 20:01:23 -0400

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "mcitstorage" {
-  name                     = var "onomestoragemay"
-  resource_group_name      = azurerm_resource_group.terraformmay022025 "onomestoragemay"
-  location                 = azurerm_resource_group.terraformmay022025.location
+  name                     = var.onomestoragemay
+  resource_group_name      = azurerm_resource_group.onomestoragemay
+  location                 = azurerm_resource_group.group.onomestoragemay.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
