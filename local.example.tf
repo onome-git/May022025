bb3501ecabb5d63411585a231d07e995b8e8034c
onome-git <reghaonome@gmail.com>
2025-05-07 20:05:18 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -3,4 +3,9 @@ locals{
   forum="abracadabramcit"
   lengthsa=length(local.servicename)
 winterlistOfSports=["icehockey","snowboarding","iceskating"]
+characters = ["luke", "yoda", "darth"]
+  enemies_destroyed = [4252, 900, 20000056894]
+character_enemy_map =   { for character in local.characters: # Convert character list to a set
+      character => local.enemies_destroyed
+}
 }
