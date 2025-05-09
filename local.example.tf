c1fc42448b712de43a0d021b2045d16cb6be06ae
onome-git <reghaonome@gmail.com>
2025-05-09 19:26:48 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -13,10 +13,6 @@ food = ["fish","fries","tea","salad","pizza"]
 character_food_map =   { for character in local.top5restaurants: # Convert character list to a set
       character => local.food
 }
-variable "sum_numbers" {
-  type    = list(string)
-  default = ["42", "77", "13", "98", "3", "64", "55", "27", "89", "6"]
-}
 
 output "total_sum" {
   value = sum([for n in var.sum_numbers : tonumber(n)])
