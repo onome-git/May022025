f170236d06a1034b50f8fefa13d3267e398168ae
onome-git <reghaonome@gmail.com>
2025-05-10 14:34:35 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -8,10 +8,14 @@ characters = ["luke", "yoda", "darth"]
 character_enemy_map =   { for character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed
 }
-top5restaurants= zipmap(["macdonald","timhorton","kingburger","subway","kfc"])
-food = ["fish","fries","tea","salad","pizza"]
+
 character_food_map =   { for character in local.top5restaurants: # Convert character list to a set
       character => local.food
+
+  top5restaurants = ["macdonald", "timhorton", "kingburger", "subway", "kfc"]
+  food            = ["fish", "fries",      "tea",        "salad",  "pizza"]
+  restaurant_food_map = zipmap(local.top5restaurants, local.food)
+}
 }
 
 
