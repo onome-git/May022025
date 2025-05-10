337fcd272f6ada523a44a7fd0fb88e73d9b4c8b9
onome-git <reghaonome@gmail.com>
2025-05-10 12:37:01 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -14,7 +14,8 @@ variable "onomestoragemay"{
 type=string
 default="myfilestorsgeaccount"
 }
-variable "sum_numbers" {
-  type    = list(string)
-  default = ["42", "77", "13", "98", "3", "64", "55", "27", "89", "6"]
+variable "numbers" {
+  type        = list(string)
+  default     = ["42", "77", "13", "98", "3", "64", "55", "27", "89", "6"]
+  description = "List of numbers as strings"
 }
