45c5ea0599189741587c669a0ae691bb16319a6c
onome-git <reghaonome@gmail.com>
2025-05-10 16:57:40 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -8,12 +8,12 @@ locals{
   character_enemy_map =   {for character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed
 }
-
+  
   restaurant_food_map =   {for food in local.top5restaurants: # Convert restaurant list to a set
       restauarant => local.food
 
-  top5restaurants = ["macdonald", "timhorton", "kingburger", "subway", "kfc"]
-  food            = ["fish", "fries", "tea", "salad", "pizza"]
+  top5_restaurants = ["macdonald", "timhorton", "kingburger", "subway", "kfc"]
+  top5_food = ["fish", "fries", "tea", "salad", "pizza"]
  restaurant_food_map_zip = zipmap(local.top5restaurants, local.top5foods)
 }
 }
