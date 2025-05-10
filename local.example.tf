cbdfb5c026bb9d815c804d170285abed4826d45f
onome-git <reghaonome@gmail.com>
2025-05-10 14:51:52 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -5,11 +5,11 @@ locals{
   winterlistOfSports=["icehockey","snowboarding","iceskating"]
   characters = ["luke", "yoda", "darth"]
   enemies_destroyed = [4252, 900, 20000056894]
-  character_enemy_map =   { for character in local.characters: # Convert character list to a set
+  character_enemy_map =   {for character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed
 }
 
-  rrestaurant_food_map =   { for food in local.top5restaurants: # Convert restaurant list to a set
+  restaurant_food_map =   {for food in local.top5restaurants: # Convert restaurant list to a set
       restauarant => local.food
 
   top5restaurants = ["macdonald", "timhorton", "kingburger", "subway", "kfc"]
