b5a8220550cc259e7c944c22484ef019a0e34e2b
onome-git <reghaonome@gmail.com>
2025-05-11 12:13:02 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -6,20 +6,23 @@ locals{
   characters = ["luke", "yoda", "darth"]
   enemies_destroyed = [4252, 900, 20000056894]
   character_enemy_map =   {for character in local.characters: # Convert character list to a set
-      character => local.enemies_destroyed
-}
+  character => local.enemies_destroyed
+  }
   
   restaurants_food_map = {for idx, restaurant in local.top5_restaurants: # Convert restaurants list to a set
-      restaurant => idx
-}
+  restaurant => idx
+  }
   top5_restaurants = ["macdonald","timhorton","kingburger","subway","kfc"] 
   top5_food = ["fish","fries","tea","salad","pizza"]
  restaurants_food_map_zip = zipmap(local.top5_restaurants, local.top5_food)
 
-}
-original_text ="IAM A STUDENT AT MCIT"
-lower_text = lower(local.original_text)
-}
+  }
+  string_numbers - ["42,"77", "13", "98", "3","64","55","27","89","6"]
+  # convert strings to numbers and sum
+  number_sum = sum([for n i local.string_numbers : tonumber(n)])
+  original_text ="IAM A STUDENT AT MCIT"
+  lower_text = lower(local.original_text)
+  }
  
 
 
