895ca73aea4b7a0e916991e4ea249c41c4e0a70d
onome-git <reghaonome@gmail.com>
2025-05-11 12:24:36 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -18,12 +18,12 @@ locals{
 
   }
   string_numbers = ["42","77", "13", "98", "3","64","55","27","89","6"]
-  # convert strings to numbers and sum
+  
   number_sum = sum([for n in local.string_numbers : tonumber(n)])
-  original_text = "I AM A STUDENT AT MCIT"
-  lower_text = lower(local.original_text)
+  
+  original_text  = "I AM A STUDENT AT MCIT"
+  lower_text    = lower(local.original_text)
   }
  
-
 
 
