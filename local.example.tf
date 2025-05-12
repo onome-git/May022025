58425a26750c71e70fddeee778b83f18c1499615
onome-git <reghaonome@gmail.com>
2025-05-11 23:16:54 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -1,29 +1,27 @@
-locals{
-  servicename="abracadabra"
-  forum="abracadabramcit"
-  lengthsa=length(local.servicename)
-  winterlistOfSports=["icehockey","snowboarding","iceskating"]
-  characters = ["luke", "yoda", "darth"]
-  enemies_destroyed = [4252, 900, 20000056894]
-  character_enemy_map =   {for character in local.characters: # Convert character list to a set
-  character => local.enemies_destroyed
-  }
-  
-  restaurant_food_map = {for restaurant in local.restaurants : restaurant => local.top5_food
+locals {
+  servicename           = "abracadabra"
+  forum                 = "abracadabramcit"
+  lengthsa              = length(local.servicename)
+  winterlistOfSports    = ["icehockey", "snowboarding", "iceskating"]
+  characters            = ["luke", "yoda", "darth"]
+  enemies_destroyed     = [4252, 900, 20000056894]
 
+  character_enemy_map = {
+    for i, character in local.characters :
+    character => local.enemies_destroyed[i]
   }
-  top5_restaurants = ["macdonald","timhorton","kingburger","subway","kfc"] 
-  top5_food = ["fish","fries","tea","salad","pizza"]
+
+  top5_restaurants      = ["macdonald", "timhorton", "kingburger", "subway", "kfc"]
+  top5_food             = ["fish", "fries", "tea", "salad", "pizza"]
+
   restaurants_food_map_zip = zipmap(local.top5_restaurants, local.top5_food)
 
-  }
-  string_numbers = ["42","77", "13", "98", "3","64","55","27","89","6"]
-  
-  number_sum = sum([for n in local.string_numbers : tonumber(n)])
-  
-  original_text  = "I AM A STUDENT AT MCIT"
+  string_numbers        = ["42", "77", "13", "98", "3", "64", "55", "27", "89", "6"]
+
+  number_sum = sum([
+    for n in local.string_numbers : tonumber(n)
+  ])
+
+  original_text = "I AM A STUDENT AT MCIT"
   lower_text    = lower(local.original_text)
-  }
- 
-
-
+}
