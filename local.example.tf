03ecc17a7a5b85b5eee6787ee1cac093da82367d
onome-git <reghaonome@gmail.com>
2025-05-13 15:12:00 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -79,10 +79,12 @@ locals {
   reversed_words_8      = reverse(local.words_8)
   sentence_8_reversed   = join(" ", local.reversed_words_8)
 }
+
 locals {
   sentence_9         = "every foodie should visit once."
   words_9            = split(" ", local.sentence_9)
-  capitalized_words  = [for word in local.words_9 : upper(substr(word, 0, 1)) .. substr(word, 1, length(word) - 1)
+  capitalized_words  = [
+    for word in local.words_9 : upper(substr(word, 0, 1)) + substr(word, 1, length(word) - 1)
   ]
   sentence_9_capitalized = join(" ", local.capitalized_words)
 }
