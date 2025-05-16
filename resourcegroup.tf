2e1a59f6809dda48a7bb260e0237f04b0c473490
onome-git <reghaonome@gmail.com>
2025-05-16 18:49:27 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,14 @@
 resource "azurerm_resource_group" "onomemay022025"{
-  name= "onomemcit_resource_group"
+  name= "onome_resource_group"
   location="canadacentral"
+}
+resource "azurerm_storage_account" "onomestoremay2025" {
+  for_each = local.full_service_names
+
+
+  name                     = each.value
+  resource_group_name      = "onomeresources"
+  location                 = "East US"
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
 }
