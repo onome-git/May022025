97ff812fbfaf99cc81ce475d8fd8d7f6e58c2456
onome-git <reghaonome@gmail.com>
2025-05-16 14:26:09 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -108,6 +108,6 @@ locals {
 }
 locals {
   sentence   = "antione sean negar ovadia."
-  words      = split(" ", trim(local.sentence, "."))
-  last_word  = local.words[length(local.words) - 1]
+  words_v2      = split(" ", trim(local.sentence, "."))
+  last_word-v2  = local.words_v2[length(local.words) - 1]
 }
