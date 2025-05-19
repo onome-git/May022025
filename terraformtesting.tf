601ba7c641a9a950cb703afb8a67eed7bbfd614d
onome-git <reghaonome@gmail.com>
2025-05-19 10:39:34 -0400

    Update terraformtesting.tf

--- terraformtesting.tf
+++ terraformtesting.tf
@@ -1,5 +1,5 @@
 locals {
   sentence   = "antione sean negar ovadia."
   wordsv2      = split(" ", trim(local.sentence, "."))
-  last_wordv2  = local.words[length(local.wordsv2) - 1]
+  last_wordv2  = local.wordsv2[length(local.wordsv2) - 1]
 }
