148228485224ec736c89b704f0a3eee4185fa1ff
onome-git <reghaonome@gmail.com>
2025-05-19 20:10:24 -0400

    Update terraformtesting.tf

--- terraformtesting.tf
+++ terraformtesting.tf
@@ -4,8 +4,8 @@ locals {
   last_wordv2  = local.wordsv2[length(local.wordsv2) - 1]
 }
 locals{
-my_sentence                       = "i love my children"
-sentence_wordv3        = split(" ", local.sentence)
-sentence_last_index    = length(local.sentence_wordv3) -1
-sentence_last_wordv3    = element(local.sentence_wordv3, local.sentence_last_index) 
+my_sentence            = "i love my children"
+  sentence_wordv3        = split(" ", local.my_sentence)
+  sentence_last_index    = length(local.sentence_wordv3) - 1
+  sentence_last_wordv3   = element(local.sentence_wordv3, local.sentence_last_index)
 }
