7f35aeaa78ab869e1aef67824051362615e11b79
onome-git <reghaonome@gmail.com>
2025-05-21 20:27:57 -0400

    Update mcityaml.tf

--- mcityaml.tf
+++ mcityaml.tf
@@ -1,4 +1,5 @@
 locals {
+
   windows_app = [for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
 
   windows_app_list = flatten([
@@ -12,11 +13,17 @@ locals {
   ])
 }
 
+resource "azurerm_resource_group" "onomemay022025v2" {
+  name     = "onomemay022025v2-rg"
+  location = "East US"
+}
+
+
 resource "azurerm_service_plan" "onomemay022025v2" {
-  for_each = { for sp in local.windows_app_list : sp.name => sp }
- 
-name                = each.value.name
-  resource_group_name  = azurerm_resource_group.onomemay022025v2.name
+  for_each            = { for sp in local.windows_app_list : sp.name => sp }
+
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.onomemay022025v2.name
   location            = azurerm_resource_group.onomemay022025v2.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
@@ -24,14 +31,12 @@ name                = each.value.name
 
 
 resource "azurerm_windows_web_app" "onomemay022025v2webapp" {
-  for_each = azurerm_service_plan.onomemay022025v2
+  for_each            = azurerm_service_plan.onomemay022025v2
 
-name                = each.value.name
-  resource_group_name  = azurerm_resource_group.onomemay022025v2.name
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.onomemay022025v2.name
   location            = azurerm_resource_group.onomemay022025v2.location
   service_plan_id     = each.value.id
 
   site_config {}
 }
-
-
