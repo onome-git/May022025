5382cfd5ddb924ff32e0cf498f69f38186c4208c
onome-git <reghaonome@gmail.com>
2025-05-23 13:44:47 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,7 +1,9 @@
-resource "azurerm_resource_group" "mcitdevrm"{
-  name= "onome_resource_group"
-  location="canadacentral"
+resource "azurerm_resource_group" "mcitdevrm" {
+  name     = "onomespmay022025"
+  location = "canadacentral"
 }
+
+
 resource "azurerm_storage_account" "mcitdevrm" {
   for_each = local.full_service_names
 
