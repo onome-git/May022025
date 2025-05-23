df501613ad34d41a1dc0e2a5e7eeb5dba08adeba
onome-git <reghaonome@gmail.com>
2025-05-23 15:34:55 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -14,7 +14,7 @@ locals{
 
 resource "azurerm_service_plan" "batcha06sp" {
   for_each            = { for sp in local.linux_app_list : sp.name => sp }
-  name                = each.value.name
+  name                = each.key
   resource_group_name = azurerm_resource_group.onomespmay022025v2.name
   location            = azurerm_resource_group.onomespmay022025v2.location
   os_type             = each.value.os_type
@@ -32,4 +32,6 @@ resource "azurerm_linux_web_app" "batcha06webapp" {
   service_plan_id     = each.value.id
 
   site_config {}
+    always_on = true 
+}
 }
