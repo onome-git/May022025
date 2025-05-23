f4f2c6d793a9e11f8e8bc10142880d67f50d7ffa
onome-git <reghaonome@gmail.com>
2025-05-23 13:34:11 -0400

    Update mcityaml.tf

--- mcityaml.tf
+++ mcityaml.tf
@@ -5,7 +5,7 @@ locals {
   windows_app_list = flatten([
     for app in local.windows_app : [
       for windowsapps in try(app.listofwindowsapp, []) : {
-        name     = firstwindowsapp 
+        name     = windowsapps.name
         os_type  = windowsapps.os_type
         sku_name = windowsapps.sku_name     
       }
@@ -14,7 +14,7 @@ locals {
 }
 
 resource "azurerm_resource_group" "onomespmay022025v2" {
-  name     =  secondwindowsapp 
+  name     = windowsapps.name
   location =  azurerm_resource_group.onomespmay022025v2.location
 }
 
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "onomespmay022025v2" {
 resource "azurerm_service_plan" "onomespmay022025v2" {
   for_each            = { for sp in local.windows_app_list : sp.name => sp }
 
-  name                = each.value.name
+  name                = windowsapps.name
   resource_group_name = azurerm_resource_group.onomespmay022025v2.name
   location            = azurerm_resource_group.onomespmay022025v2.location
   os_type             = each.value.os_type
@@ -33,7 +33,7 @@ resource "azurerm_service_plan" "onomespmay022025v2" {
 resource "azurerm_windows_web_app" "onomespmay022025v2webapp" {
   for_each            = azurerm_service_plan.onomemay022025v2
 
-  name                = thirdwindowsapp
+  name                = windowsapps.name
   resource_group_name = azurerm_resource_group.onomespmay022025v2.name
   location            = azurerm_resource_group.onomemay022025v2.location
   service_plan_id     = each.value.id
