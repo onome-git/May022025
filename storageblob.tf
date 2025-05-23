cf8d70eeaa46233812e3008582cff987fd42e36d
onome-git <reghaonome@gmail.com>
2025-05-23 15:21:40 -0400

    Update storageblob.tf

--- storageblob.tf
+++ storageblob.tf
@@ -1,6 +1,7 @@
 resource "azurerm_storage_blob" "onomecontainer" {
   name                   = "my-awesome-content.zip"
-  storage_account_name   = azurerm_storage_account.mcitstorage.name
+  
+  storage_account_name = azurerm_storage_account.mcitstorage_v2.name
   storage_container_name = azurerm_storage_container.onomecontainer.name
   type                   = "Block"
   source                 = "some-local-file.zip"
