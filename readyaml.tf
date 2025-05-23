0626d58c38316d3451c56205f7a773154cf89238
onome-git <reghaonome@gmail.com>
2025-05-23 13:21:45 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -1,6 +1,6 @@
 
 locals{
-  linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
+  linux_app=[for f in fileset("${path.module}/var.configs_folder", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   linux_app_list = flatten([
     for app in local.linux_app : [
       for linuxapps in try(app.listoflinuxapp, []) :{
