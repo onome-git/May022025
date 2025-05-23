7c26294f005996d37e140501db87409b2b9df497
onome-git <reghaonome@gmail.com>
2025-05-23 12:38:51 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -1,5 +1,4 @@
 
-#test
 locals{
   linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   linux_app_list = flatten([
@@ -13,20 +12,20 @@ locals{
 ])
 
 
-resource "azurerm_service_plan" "batcha06sp" {
-  for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
-  name                = firstlinuxapp
+esource "azurerm_service_plan" "batcha06sp" {
+  for_each            = { for sp in local.linux_app_list : sp.name => sp }
+  name                = each.value.name
   resource_group_name = azurerm_resource_group.onomemaysp022025.name
   location            = azurerm_resource_group.onomemaysp022025.location
-  os_type             =linux
-  sku_name            = p1v2
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
 }
 
 resource "azurerm_linux_web_app" "batcha06webapp" {
   for_each            = azurerm_service_plan.batcha06sp
-  name                = thirdliuxapp
-  os_type             =linux
-  sku_name            = p1v2
+  name                = each.value.name
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
 
   resource_group_name = azurerm_resource_group.onomespmaysp022025.name
   location            = azurerm_resource_group.onomespmaysp022025.location
