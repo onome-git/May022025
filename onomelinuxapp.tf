958720a7e64576a09504578efe112a43f238a3e0
onome-git <reghaonome@gmail.com>
2025-05-23 19:39:20 -0400

    Update onomelinuxapp.tf

--- onomelinuxapp.tf
+++ onomelinuxapp.tf
@@ -10,12 +10,14 @@ resource "azurerm_service_plan" "batcha06sp" {
 resource "azurerm_linux_web_app" "batcha06webapp" {
   for_each            = azurerm_service_plan.batcha06sp
 
-  name                = "onome-${each.value.name}" 
+  name                = "onomelinuxwebapp-${each.value.name}"  # <-- Prefix here
   resource_group_name = azurerm_resource_group.onomespmay022025v2.name
   location            = azurerm_resource_group.onomespmay022025v2.location
   service_plan_id     = each.value.id
 
   site_config {
     always_on = true
+  }
+
   }
 }
