c5b019d5a5782674f6cffdeefcb14fcd468ebac3
onome-git <reghaonome@gmail.com>
2025-05-23 13:23:27 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -15,7 +15,7 @@ locals{
 resource "azurerm_service_plan" "batcha06sp" {
   for_each            = { for sp in local.linux_app_list : sp.name => sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.onomspemay022025.name
+  resource_group_name = azurerm_resource_group.onomspmay022025.name
   location            = azurerm_resource_group.onomespmay022025.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
