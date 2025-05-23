62bdf3a6eed7df1d56126a06a5fb9b58138c0c3b
onome-git <reghaonome@gmail.com>
2025-05-23 14:07:07 -0400

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -4,14 +4,15 @@ resource "azurerm_storage_account" "mcitstorage" {
   location                 = azurerm_resource_group.onomespmay022025.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
 
 resource "azurerm_storage_account" "mcitdevrm" {
   for_each = local.full_service_names
 
-  name                = each.value
-  resource_group_name = azurerm_resource_group.mcitdevrm.name     # refer to the declared resource group
-  location            = azurerm_resource_group.mcitdevrm.location # same here
-  account_tier        = "Standard"
+  name                     = each.value
+  resource_group_name      = azurerm_resource_group.mcitdevrm.name
+  location                 = azurerm_resource_group.mcitdevrm.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
  tags = {
