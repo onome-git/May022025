8c7ada91481cbb970ac274eb9a8ebced0facbbf8
onome-git <reghaonome@gmail.com>
2025-05-23 20:11:40 -0400

    Update locals kubernetes.tf

--- locals kubernetes.tf
+++ locals kubernetes.tf
@@ -1,1 +1,25 @@
-
+locals {
+  node_pools = {
+    prod = {
+      vm_size    = "Standard_DS2_v2"
+      node_count = 1
+      tags = {
+        Environment = "prod"
+      }
+    }
+    dev = {
+      vm_size    = "Standard_D4_v3"
+      node_count = 2
+      tags = {
+        Environment = "dev"
+      }
+    }
+    qa = {
+      vm_size    = "Standard_B2s"
+      node_count = 1
+      tags = {
+        Environment = "qa"
+      }
+    }
+  }
+}
