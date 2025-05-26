dc1cb91ace677c925a94a8ce9f9a5835d0321dc7
onome-git <reghaonome@gmail.com>
2025-05-26 19:57:22 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -171,3 +171,71 @@ locals {
 locals {
   cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  vm_names = ["webvm-1", "webvm-2", "webvm-3"]
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  for_each = toset(local.vm_names)
+
+  name                = "${each.key}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  for_each = toset(local.vm_names)
+
+  name                = each.key
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  
+  network_interface_ids = [
+    azurerm_network_interface.example[each.key].id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
