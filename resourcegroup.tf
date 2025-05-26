f71902ecd2243c00ee650d9715eb8d46fe7c3ab0
onome-git <reghaonome@gmail.com>
2025-05-26 19:28:28 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -17,3 +17,18 @@ resource "azurerm_storage_account" "mcitdevrm" {
   }
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+  resource_group_name = "example-rg"
+}
+
+resource "azurerm_subnet" "subnets" {
+  for_each = { for idx, cidr in local.cidrs : idx => cidr }
+
+  name                 = "subnet-${each.key}"
+  resource_group_name  = azurerm_virtual_network.example.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = [each.value]
+}
