38e5d1d21713c7360c21d7c7ebb4b4fee49f89db
onome-git <reghaonome@gmail.com>
2025-05-26 18:35:18 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -142,3 +142,13 @@ locals {
     "vm_${idx}" => name
   }
 }
+
+locals {
+  rg_names = ["dev-rg", "test-rg", "prod-rg"]
+}
+
+resource "azurerm_resource_group" "rgs" {
+  for_each = toset(local.rg_names)
+  name     = each.value
+  location = "East US"
+}
