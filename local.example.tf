c229b54c09320316aed00d3047a85db0e92ad1ce
onome-git <reghaonome@gmail.com>
2025-05-26 20:05:33 -0400

    Update local.example.tf

--- local.example.tf
+++ local.example.tf
@@ -184,7 +184,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "example_v2-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
