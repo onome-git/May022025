9aead390072b090f3463da39b90491c78ecd0f8a
onome-git <reghaonome@gmail.com>
2025-05-28 11:53:18 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,50 +1,63 @@
 variable "subscription_id"{
   type=string
   }
+
 variable "client_id"{
   type=string
   }
+
 variable "client_secret"{
   type=string
   }
+
 variable "tenant_id"{
   type=string
   }
+
 variable "onomestoragemay"{ 
 type=string
 default="myfilestorsgeaccount"
 }
+
 variable "string_numbers" {
   type=list(string)
   default=["42", "77", "13", "98", "3", "64", "55", "27", "89", "6"]
 }
+
 variable "sentence"{
 type=string
 description ="A sentence to convert to uppercase"
 default="my love"
 }
+
 variable "full_text" {
 type=string
 default= "i love the country called canada, it is awesome"
 }
+
 variable "my_text" {
 type=string
 default = "my name is onome"
 }
- variable "trimmed_word" {
+
+variable "trimmed_word" {
   type    = string
   default = "  I love Terraform  "
 }
+
 variable "terms" {
   default = "Smoked meat is not a sandwich. It's a religion."
 }
+
 variable "sentence_4" {
   default = "Why does St-Laurent never sleep?"
 }
+
 variable "sentence_2" {
   type    = string
   default = "Poutine should be a food group."
 }
+
 variable "servicename" {
   default = "abracadabra"
 }
@@ -52,6 +65,7 @@ variable "servicename" {
 variable "env" {
   default = "non-prod"
 }
+
 variable "service_name" {
   default = "onome"
 }
@@ -63,6 +77,7 @@ variable "configs_folder" {
   type        = string
   default     = "configs"  
 }
+
 variable "folderlocation" {
   type        = string
   default     = "windows_configs" 
