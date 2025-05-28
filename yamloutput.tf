6c84c2b904d83f838b54136ab61ce37be7ca097c
onome-git <reghaonome@gmail.com>
2025-05-28 12:28:23 -0400

    Update yamloutput.tf

--- yamloutput.tf
+++ yamloutput.tf
@@ -1,1 +1,11 @@
+output "subnet_names" {
+  value = [for k in keys(local.subnets) : k]
+}
 
+output "subnet_cidrs" {
+  value = [for v in values(local.subnets) : v]
+}
+
+output "subnet_map" {
+  value = local.subnets
+}
