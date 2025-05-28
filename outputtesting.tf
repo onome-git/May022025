2a235dddc48e7b312eb04908cb4bc9e9161cbd70
onome-git <reghaonome@gmail.com>
2025-05-28 11:40:25 -0400

    Update outputtesting.tf

--- outputtesting.tf
+++ outputtesting.tf
@@ -53,14 +53,4 @@ output "vm_private_ips" {
 }
 
 
-output "region" {
-  value = local.region
-}
 
-output "is_primary_region" {
-  value = local.is_primary
-}
-
-output "environment" {
-  value = local.environment
-}
