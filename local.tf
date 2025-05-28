df86ec2467f0a0b3806f1ff3e82753187ee96046
onome-git <reghaonome@gmail.com>
2025-05-28 11:43:17 -0400

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,17 +1,1 @@
-locals {
-  region       = "eastus"
-  is_primary   = local.region == "eastus" ? true : false
-  environment  = local.is_primary ? "production" : "staging"
-}
 
-output "region" {
-  value = local.region
-}
-
-output "is_primary_region" {
-  value = local.is_primary
-}
-
-output "environment" {
-  value = local.environment
-}
