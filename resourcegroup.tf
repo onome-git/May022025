e3f25e39ed7908e23a848ffdda839ab47d30bfea
onome-git <reghaonome@gmail.com>
2025-05-28 12:44:38 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -43,7 +43,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-{
+}
 
 resource "azurerm_subnet" "subnet" {
   for_each             = local.subnets
