df3103b161e2b3af215ba63fa1e0d0337b706dc9
onome-git <reghaonome@gmail.com>
2025-06-02 19:12:22 -0400

    Create output.tf

---
+++ module/storage_account/output.tf
@@ -0,0 +1,7 @@
+output "storage_account_id" {
+  value = azurerm_storage_account.mcitstoragemodule.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.mcitstoragemodule.name
+}
