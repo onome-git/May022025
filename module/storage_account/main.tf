d80f47feba78ddce13fe1ad569903091f9d32b3d
onome-git <reghaonome@gmail.com>
2025-06-02 19:05:36 -0400

    Create main.tf

---
+++ module/storage_account/main.tf
@@ -0,0 +1,10 @@
+esource "azurerm_storage_account" "mcitstoragemodule" {
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  access_tier              = var.access_tier
+
+  tags = var.tags
+}
