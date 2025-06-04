5f96a0a44f81d97b274da572e343f420d906246b
onome-git <reghaonome@gmail.com>
2025-06-04 19:44:44 -0400

    Update mcitstoragemodulecall.tf

--- mcitstoragemodulecall.tf
+++ mcitstoragemodulecall.tf
@@ -14,22 +14,7 @@ module "mymcitstorage_v4" {
   }
 }
 
-module "mymcitstorage_v5" {
-  source = "./modules/storage_account"
 
-  for_each = var.storage_accounts  # Loops over the map to create 4 storage accounts
-
-  
-
-module "mymcitstorage_v5" {
-  source                    = "./modules/storage_account"
-  name                      = "onomestorageacct5"
-  resource_group_name       = azurerm_resource_group.mcit420zz5um.name
-  location                  = azurerm_resource_group.mcit420zz5um.location
-  account_tier              = "Standard"
-  account_replication_type  = "GRS"
-  kind                      = "StorageV2"
-  access_tier               = "Hot"
 
   tags = {
     environment = "dev"
