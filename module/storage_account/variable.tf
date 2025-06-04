cf1f62572cdc53585f1a7532cf9095bffa7d7fcc
onome-git <reghaonome@gmail.com>
2025-06-04 17:43:04 -0400

    Update variable.tf

--- module/storage_account/variable.tf
+++ module/storage_account/variable.tf
@@ -42,3 +42,35 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+ariable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
+variable "kind" {
+  type = string
+}
+
+variable "access_tier" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
