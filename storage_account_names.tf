1aa1c2459052d26733ce3aed19a2e6b309f7790a
onome-git <reghaonome@gmail.com>
2025-06-04 20:10:11 -0400

    Update storage_account_names.tf

--- storage_account_names.tf
+++ storage_account_names.tf
@@ -1,16 +1,1 @@
 
-module "mymcitstorage_v3" {
-  source                    = "./modules/storage_account"
-  name                      = "onomestorageacct3"
-  resource_group_name       = azurerm_resource_group.mcit420zz5um.name
-  location                  = azurerm_resource_group.mcit420zz5um.location
-  account_tier              = "Standard"
-  account_replication_type  = "ZRS"
-  kind                      = "StorageV2"
-  access_tier               = "Hot"
-
-  tags = {
-    environment = "dev"
-    team        = "infra"
-  }
-}
