8bd08bafba4e0339cf6d8a37f984a070d3479c60
onome-git <reghaonome@gmail.com>
2025-06-04 21:53:50 -0400

    Update mcitstoragemodulecall.tf

--- mcitstoragemodulecall.tf
+++ mcitstoragemodulecall.tf
@@ -1,14 +1,48 @@
-module "mymcitstorage_1" {
-  source = "./modules/storage_account" 
-  
-  name                    = "onomestorageacct4"
-  resource_group_name     = azurerm_resource_group.mcit420zz5um.name
-  location                = azurerm_resource_group.mcit420zz5um.location
-  account_tier            = "Standard"
-  account_replication_type = "LRS"
-  kind                    = "StorageV2"
-  access_tier             = "Cool"
+ocals {
+  storage_accounts = {
+    sa1 = {
+      name                     = "onomestorageacct"
+      account_tier             = "Standard"
+      account_replication_type = "LRS"
+      kind                     = "StorageV2"
+      access_tier              = "Hot"
+    }
+    sa2 = {
+      name                     = "onomestorageacct2"
+      account_tier             = "Standard"
+      account_replication_type = "GRS"
+      kind                     = "StorageV2"
+      access_tier              = "Cool"
+    }
+  }
+}
 
+module "mymcitstorage" {
+  for_each = local.storage_accounts
+
+  source              = "./modules/storage_account"
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  account_tier        = each.value.account_tier
+  account_replication_type = each.value.account_replication_type
+  kind                = each.value.kind
+  access_tier         = each.value.access_tier
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
+
+module "myforeachmcitstorage" {
+  source              = "./modules/storage_account"
+  name                = "onomestorageacct"
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  account_tier        = "Standard"
+  account_replication_type = "LRS"
+  kind                = "StorageV2"
+  access_tier         = "Hot"
   tags = {
     environment = "dev"
     team        = "infra"
