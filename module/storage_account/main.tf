ffae2b6b16cbd65837c39b0bce192e125d134529
onome-git <reghaonome@gmail.com>
2025-06-05 12:02:38 -0400

    Update main.tf

--- module/storage_account/main.tf
+++ module/storage_account/main.tf
@@ -1,14 +1,3 @@
-resource "azurerm_storage_account" "mcitstoragemodule" {
-  name                     = var.name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  access_tier              = var.access_tier
-
-  tags = var.tags
-}
-
 
 resource "azurerm_storage_account" "this" {
   name                     = var.name
