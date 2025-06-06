dd2e3a1bc9890f665d0eabf49c0ba535481b9727
onome-git <reghaonome@gmail.com>
2025-06-06 10:22:13 -0400

    Update mcitstoragemodulecall.tf

--- mcitstoragemodulecall.tf
+++ mcitstoragemodulecall.tf
@@ -1,7 +1,7 @@
 module "mymcitstorage_group1" {
   for_each = local.storage_accounts
 
-  source              = "./modules/storage_account"
+  source              = "./modules/storage_account_group1"
   name                = each.value.name
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
   location            = azurerm_resource_group.mcit420zz5um.location
