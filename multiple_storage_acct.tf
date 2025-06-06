39423b1a55ab4943e572f87595c144ee8801668e
onome-git <reghaonome@gmail.com>
2025-06-06 10:43:09 -0400

    Update multiple_storage_acct.tf

--- multiple_storage_acct.tf
+++ multiple_storage_acct.tf
@@ -40,25 +40,9 @@ locals {
 }
 
 
-module "mymcitstorage_group2" {
-  for_each = local.storage_accounts
-
-  source              = "./modules/storage_account"
-  name                = each.value.name
-  resource_group_name = azurerm_resource_group.mcit420zz5um.name
-  location            = azurerm_resource_group.mcit420zz5um.location
-  account_tier        = each.value.account_tier
-  account_replication_type = each.value.account_replication_type
-  kind                = each.value.kind
-  access_tier         = each.value.access_tier
-  tags = {
-    environment = "dev"
-    team        = "infra"
-  }
-}
 
 module "mymcitstorage_group2" {
-  source              = "./module/storage_account"  # ✅ Match actual folder
+  source              = "./module/storage_account"  
   name                = each.value.name
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
   location            = azurerm_resource_group.mcit420zz5um.location
