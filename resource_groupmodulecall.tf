ae0d4d6b70a7923e83d3812acfc4b1cf0b1ad467
onome-git <reghaonome@gmail.com>
2025-06-07 02:28:14 -0400

    Update resource_groupmodulecall.tf

--- resource_groupmodulecall.tf
+++ resource_groupmodulecall.tf
@@ -1,8 +1,6 @@
 
 module "resource_group" {
-  for_each = var.resource_groups
-
-  source   = "./modules/resource_group"
-  name     = each.key
-  location = each.value.location
+  source = "./modules/resource_group"  
+  name   = "onome-rg"
+  location = "eastus"
 }
