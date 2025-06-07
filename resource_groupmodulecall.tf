7ede0b88f5a17b1d05280dae1f27bb3b8e98b5d7
onome-git <reghaonome@gmail.com>
2025-06-07 18:28:46 -0400

    Update resource_groupmodulecall.tf

--- resource_groupmodulecall.tf
+++ resource_groupmodulecall.tf
@@ -1,8 +1,20 @@
 
-module "resource_group" {
+locals {
+  resource_groups = {
+    rg-onome-eastus     = "eastus"
+    rg-onome-westus     = "westus"
+    rg-onome-central    = "centralus"
+    rg-onome-canada     = "canadacentral"
+    rg-onome-canadaeast = "canadaeast"
+  }
+}
+
+module "resource_groups" {
+  for_each = local.resource_groups
+
   source   = "./modules/resource_group"
-  name     = "rg-onome-eastus"
-  location = "eastus"
+  name     = each.key
+  location = each.value
   tags = {
     environment = "dev"
     team        = "infra"
