64ed986d1923a6ae0b9e024f2524b6c846243832
onome-git <reghaonome@gmail.com>
2025-06-07 17:48:39 -0400

    Update mcitstoragemodulecall.tf

--- mcitstoragemodulecall.tf
+++ mcitstoragemodulecall.tf
@@ -29,13 +29,3 @@ module "onomeforeachmcitstorage" {
 }
 
 
-module "my_resource_group" {
-  source   = "./modules/resource_group"
-  name     = "rg-onome-eastus"
-  location = "eastus"
-  tags = {
-    environment = "dev"
-    team        = "infra"
-  }
-}
-
