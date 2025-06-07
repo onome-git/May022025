58065d71a738c00322b4fb80382c96e882cb3e1f
onome-git <reghaonome@gmail.com>
2025-06-07 03:18:28 -0400

    Update multiple_resource_group.tf

--- multiple_resource_group.tf
+++ multiple_resource_group.tf
@@ -1,15 +1,16 @@
+
 locals {
-    multipleresource_groups = {
-    rg-onome-eastus   = "eastus"
-    rg-onome-westus   = "westus"
-    rg-onome-central  = "centralus"
-    rg-onome-canada   = "canadacentral"
-    rg-onome-canadaeast  = "canadaeast"
+  multipleresource_groups = {
+    rg-onome-eastus     = "eastus"
+    rg-onome-westus     = "westus"
+    rg-onome-central    = "centralus"
+    rg-onome-canada     = "canadacentral"
+    rg-onome-canadaeast = "canadaeast"
   }
 }
 
-module "resource_groups" {
-  for_each = multipleresource_groups
+module "multiple_resource_groups" {
+  for_each = local.multipleresource_groups
 
   source   = "./modules/resource_group"
   name     = each.key
