c5d41fcad3f8d1f5fd3e7e3e83c5e77ee76ba37b
onome-git <reghaonome@gmail.com>
2025-06-07 01:17:17 -0400

    Create output.tf

---
+++ module/resource_group/output.tf
@@ -0,0 +1,13 @@
+variable "name" {
+  description = "The name of the storage account."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where the storage account will be created."
+  type        = string
