42aa84a07ca624effad5856e976e6285edbe9b0c
onome-git <reghaonome@gmail.com>
2025-06-07 01:41:42 -0400

    Update multiple_resource_group.tf

--- multiple_resource_group.tf
+++ multiple_resource_group.tf
@@ -1,1 +1,21 @@
+locals {
+  resource_groups = {
+    rg-onome-eastus   = "eastus"
+    rg-onome-westus   = "westus"
+    rg-onome-central  = "centralus"
+    rg-onome-canada   = "canadacentral"
+    rg-onome-canadaeast  = "canadaeast"
+  }
+}
 
+module "resource_groups" {
+  for_each = local.resource_groups
+
+  source   = "./modules/resource_group"
+  name     = each.key
+  location = each.value
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
