4c659d3314afb266bc61e0571e9994166911c8a4
onome-git <reghaonome@gmail.com>
2025-06-07 17:37:38 -0400

    Update resource_groupmodulecall.tf

--- resource_groupmodulecall.tf
+++ resource_groupmodulecall.tf
@@ -1,6 +1,6 @@
 
-module "single_resource_group" {
-  source   = "./modules/single_resource_group"
+module "resource_group" {
+  source   = "./modules/resource_group"
   name     = "rg-onome-eastus"
   location = "eastus"
   tags = {
