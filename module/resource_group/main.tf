88218f24904c471dc7cebbafa3bd7ea1c61874af
onome-git <reghaonome@gmail.com>
2025-06-07 18:27:48 -0400

    Update main.tf

--- module/resource_group/main.tf
+++ module/resource_group/main.tf
@@ -5,24 +5,3 @@ resource "azurerm_resource_group" "rg" {
   tags     = var.tags
 }
 
-locals {
-  resource_groups = {
-    rg-onome-eastus     = "eastus"
-    rg-onome-westus     = "westus"
-    rg-onome-central    = "centralus"
-    rg-onome-canada     = "canadacentral"
-    rg-onome-canadaeast = "canadaeast"
-  }
-}
-
-module "resource_groups" {
-  for_each = local.resource_groups
-
-  source   = "./modules/resource_group"
-  name     = each.key
-  location = each.value
-  tags = {
-    environment = "dev"
-    team        = "infra"
-  }
-}
