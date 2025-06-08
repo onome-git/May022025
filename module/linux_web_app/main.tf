dd2fe058f6c73ca3b9ca577c3f5f87e2367d17cd
onome-git <reghaonome@gmail.com>
2025-06-08 11:05:13 -0400

    Create main.tf

---
+++ module/linux_web_app/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_service_plan" "lynchsp05062025" {
+  name                = var.service_plan_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  os_type             = "Linux"
+  sku_name            = var.service_plan_sku_name
+}
+
+resource "azurerm_linux_web_app" "lynchlinuxwebapp06052025" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  service_plan_id     = azurerm_service_plan.lynchsp05062025.id
+  tags                = var.tags
+
+  site_config {
+    always_on     = var.site_config.always_on
+    http2_enabled = var.site_config.http2_enabled
+  }
+
+  app_settings = var.app_settings
+
+  identity {
+    type = var.identity_type
+  }
+}
