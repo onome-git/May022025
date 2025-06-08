c85d4e14a6587695fc1e403ec6cd4a74474138e0
onome-git <reghaonome@gmail.com>
2025-06-08 11:16:42 -0400

    Create output.tf

---
+++ module/linux_web_app/output.tf
@@ -0,0 +1,7 @@
+output "web_app_id" {
+  value = azurerm_linux_web_app.lynchlinuxwebapp06052025.id
+}
+
+output "default_hostname" {
+  value = azurerm_linux_web_app.lynchlinuxwebapp06052025.default_hostname
+}
