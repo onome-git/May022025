42f6cbf97d603acd7d1243100b8a888c816948be
onome-git <reghaonome@gmail.com>
2025-06-08 11:01:54 -0400

    Update output.tf

--- module/resource_group/output.tf
+++ module/resource_group/output.tf
@@ -1,7 +1,7 @@
 output "name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.onomerg2025.name
 }
 
 output "location" {
-  value = azurerm_resource_group.rg.location
+  value = azurerm_resource_group.onomerg2025.location
 }
