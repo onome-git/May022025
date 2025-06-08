5d34c2915e2e97cdfc322b9e2f888a7fab6ed76b
onome-git <reghaonome@gmail.com>
2025-06-08 11:18:09 -0400

    Update output.tf

--- module/linux_web_app/output.tf
+++ module/linux_web_app/output.tf
@@ -1,7 +1,7 @@
 output "web_app_id" {
-  value = azurerm_linux_web_app.lynchlinuxwebapp06052025.id
+  value = azurerm_linux_web_app.onomelinuxwebapp06052025.id
 }
 
 output "default_hostname" {
-  value = azurerm_linux_web_app.lynchlinuxwebapp06052025.default_hostname
+  value = azurerm_linux_web_app.onomelinuxwebapp06052025.default_hostname
 }
